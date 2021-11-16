fa59effe014d0c2be08eebae14439f25a872f958
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-16 12:11:22 +0100

    Add initial setup for Expiration service.

---
+++ expiration/src/index.ts
@@ -0,0 +1,29 @@
+import natsWrapper from "./nats-wrapper";
+
+const start = async () => {
+  const variableNames = ["NATS_CLIENT_ID", "NATS_URL", "NATS_CLUSTER_ID"];
+
+  variableNames.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`${key} env variable should be defined.`);
+    }
+  });
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
