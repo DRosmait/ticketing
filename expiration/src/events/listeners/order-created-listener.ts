ba0cbb3997218f13cc1b327dc64ac90006d77a27
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-16 13:52:02 +0100

    Use OrderCreatedListener in Expiration service.

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,22 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@amid3ntickets/common";
+import { expirationQueue } from "../../queues";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = "expiration-service";
 
-  onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
+    msg.ack();
   }
 }
