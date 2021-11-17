df984f2e169ccd33d7d884d32847306a2c9d5a59
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-17 20:40:14 +0100

    Add publisher and listener to index files.

--- orders/src/events/index.ts
+++ orders/src/events/index.ts
@@ -1,6 +1,7 @@
 export * from "./listeners/ticket-created-listener";
 export * from "./listeners/ticket-updated-listener";
 export * from "./listeners/expiration-complete-event";
+export * from "./listeners/payment-created-listener";
 
 export * from "./publishers/order-created-publisher";
 export * from "./publishers/order-cancelled-publisher";
