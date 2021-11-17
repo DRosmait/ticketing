a3e861d9b79eaf24127abac34de389816ffe637f
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-17 20:39:28 +0100

    Use PaymentCreatedListener.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,6 +6,7 @@ import {
   TicketCreatedListener,
   TicketUpdatedListener,
   ExpirationCompleteListener,
+  PaymentCreatedListener,
 } from "./events";
 
 const start = async () => {
@@ -39,6 +40,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected to MongoDB");
