0ddd40265413ced70a439870b6e1d134ed82b596
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-18 16:37:19 +0100

    Adjust client service.

---
+++ client/pages/orders/index.js
@@ -0,0 +1,25 @@
+const OrderIndex = ({ orders }) => {
+  return (
+    <div>
+      <h1>Orders</h1>
+
+      <ul>
+        {orders.map((order) => {
+          return (
+            <li key={order.id}>
+              Order for ticket: {order.ticket.title} - {order.status}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+OrderIndex.getInitialProps = async (context, client) => {
+  const { data } = await client.get("/api/orders");
+
+  return { orders: data };
+};
+
+export default OrderIndex;
