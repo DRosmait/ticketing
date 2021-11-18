c7d5486b577c2447ffa83fbf06b18e5df63e431c
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-18 18:49:22 +0100

    Fix test

--- auth/src/routes/__test__/currentuser.spec.ts
+++ auth/src/routes/__test__/currentuser.spec.ts
@@ -10,7 +10,7 @@ describe("currentuser.ts", () => {
       .get("/api/users/currentuser")
       .set("Cookie", cookie)
       .send()
-      .expect(400);
+      .expect(200);
 
     expect(response.body.currentUser.email).toEqual("test@test.com");
   });
